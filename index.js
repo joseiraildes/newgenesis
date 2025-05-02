fe09ebe5c8d646b4a1a155d55c83df3668b4bbed
joseiraildes <joseiraildesciprianoribeiro1@gmail.com>
2025-05-02 11:16:04 -0300

    refactor(index): remove unused server and config code

--- index.js
+++ index.js
@@ -1,9 +1,2 @@
-const { app } = require("./config/config.js");
-const server = require("./socket/config.js");
 
-require("./routes.js");
-
-server.listen(3000, (err) => {
-    if (err) console.log(err);
-    console.log("Server is running on port 3000");
-});
+require("./routes.js")
