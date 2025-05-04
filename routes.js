3ee25fa25c724f1aa0d697c982f2af9ca634aa3f
joseiraildes <joseiraildesciprianoribeiro1@gmail.com>
2025-05-04 11:31:20 -0300

    feat(auth): adicionar lógica de login e registro com validação de usuário e email

--- routes.js
+++ routes.js
@@ -6,6 +6,7 @@ const hbs = require("express-handlebars")
 const path = require("path")
 const IpQuery = require("./ip/api.js")
 const User = require("./models/User.js")
+const Datetime = require("./moment/config.js")
 
 const app = express()
 const server = createServer(app)
@@ -42,7 +43,7 @@ app.get("/", async(req, res) => {
     // })
     res.redirect("/login")
   } else {
-    res.render("home")
+    res.render("home", { user: user.dataValues })
     console.log("User found =>", user.dataValues)
   }
   
@@ -66,6 +67,43 @@ app.get("/login", async(req, res)=>{
     console.log("User found =>", user.dataValues)
   }
 })
+app.post("/login", async(req, res)=>{
+  // login
+  const ip = await IpQuery()
+  const { username, password } = req.body
+
+  console.log("IP =>", ip)
+  console.log("Username =>", username)
+  console.log("Password =>", password)
+  
+
+
+  const user = await User.findOne({ where: { username: username } });
+  if (user && user.password === password) {
+    console.log("User found =>", user.dataValues)
+    const update = await User.update({
+      ip: ip,
+      data: Datetime()
+    }, {
+      where: {
+        username: username,
+      }
+    })
+
+    console.log("User logged in =>", username);
+    res.redirect("/");
+  } else {
+
+    const error = `
+      <div class="alert alert-warning alert-dismissible fade show" role="alert">
+        <strong>Opa, houve um erro aqui!</strong> Campos de usuário ou senha, não estão corretos.
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+      </div>`;
+    res.render("login", { error: error });
+  }
+  
+  
+})
 app.get("/register", async(req, res)=>{
   // register
   const ip = await IpQuery()
@@ -84,6 +122,59 @@ app.get("/register", async(req, res)=>{
     console.log("User found =>", user.dataValues)
   }
 })
+app.post("/register", async(req, res)=>{
+  // register
+  const ip = await IpQuery()
+  const { username, password, email } = req.body
+
+  console.log("IP =>", ip)
+  console.log("Username =>", username)
+  console.log("Password =>", password)
+  console.log("Email =>", email)
+
+  const user = await User.findOne({
+    where: {
+      ip: ip
+    }
+  })
+  // check if username and email are registed
+  const nameCheck = await User.findOne({
+    where: {
+      username: username
+    }
+  })
+  const emailCheck = await User.findOne({
+    where: {
+      email: email
+    }
+  })
+  if (!nameCheck || nameCheck === null || nameCheck === undefined && !emailCheck || emailCheck === null || emailCheck === undefined) {
+    // create user
+    const newUser = await User.create({
+      username: username,
+      password: password,
+      email: email,
+      data: Datetime(),
+      ip: ip,
+      bio: ""
+    })
+    console.log("User created =>", newUser.dataValues)
+    // res.json({
+    //   message: "User created",
+    //   status: true
+    // })
+    res.redirect("/login")
+  } else {
+    console.log("User already exists")
+    const error = `
+      <div class="alert alert-danger alert-dismissible fade show" role="alert">
+        <strong>Opa, houve um erro aqui!</strong> Campos inscritos como: usuário e email, já estão cadastrados em NewGenesis.
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+      </div>`;
+    res.render("register", { error: error })
+  }
+})
+
 // socket.io connection
 io.on("connection", (socket) => {
   console.log("New client connected =>", socket.id)
